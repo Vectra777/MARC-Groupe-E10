c5d1ad32197f14cab6502e4d4b87fd646d28b1c7
crafiouse <crafiouse@gmail.com>
2024-10-21 18:26:27 +0200

    tree

---
+++ tree.h
@@ -0,0 +1,20 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include "map.h"
+
+typedef struct {
+    Node* root;
+} Tree;
+
+typedef struct Node {
+    int x, y; // Position de MARC
+    int cost;
+    int totalCost;
+    t_soil soilType;
+
+    struct Node** children;
+} Node;
+
+
+#endif //TREE_H
